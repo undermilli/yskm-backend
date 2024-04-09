995904dff582b50e5ac56218be359c62344480a9
axel guichard <axelguichard.45@gmail.com>
2024-04-09 11:05:40 +0900

    fix(BE): use env for DB url, use distincts answers and clean unused code

--- routes/data.route.js
+++ routes/data.route.js
@@ -19,25 +19,8 @@ router
   .route("/set-tier")
   .post(authMiddleware, tryCatch(DataController.setTier));
 router
-  .route("/get-questions")
-  .get(authMiddleware, tryCatch(DataController.getQuestions));
-router
   .route("/update-tier-and-score")
   .post(authMiddleware, tryCatch(DataController.updateTierAndScore));
-router
-  .route("/get-multiple-choice-answers")
-  .get(authMiddleware, tryCatch(DataController.getMultipleChoiceAnswers));
-router
-  .route("/update-questions-in-database")
-  .post(authMiddleware, tryCatch(DataController.updateQuestionsInDatabase));
-
-router
-  .route("/filter-questions-based-on-tiers")
-  .get(authMiddleware, tryCatch(DataController.filterQuestionsBasedOnTier));
-router
-  .route("/test-how-many-questions-are-there")
-  .get(authMiddleware, tryCatch(DataController.testHowManyDocs));
-
 router
   .route("/send-question-to-frontend")
   .get(authMiddleware, tryCatch(DataController.sendQuestionToFrontend));
